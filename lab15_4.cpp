388e9e5d706ee43bb5860e933922d635a53c1388
fUuiyaa <kengwanitchitsanupong@gmail.com>
2025-01-28 16:24:21 +0700

    completed

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,27 @@ int main(){
 }
 
 //Do not modify source code above this line
+
+void shuffle(int &a, int &b, int &c, int &d) {
+    int temp[4]= {a, b, c, d};
+    bool used[4]= {false}; 
+
+    for (int i=0; i<4; i++) {
+        int index;
+        do {
+            index = rand()%4;
+        } while (used[index]);
+
+        
+        temp[i] = (index == 0) ? a : (index == 1) ? b : (index == 2) ? c : d;
+
+        
+        used[index]= true;
+    }
+
+    
+    a=temp[0];
+    b=temp[1];
+    c=temp[2];
+    d=temp[3];
+}
